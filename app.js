7db88a5caffa3d0476ba1d56a6727618cb360096
Jesie Oldenburg <kitglo46@gmail.com>
2019-05-22 13:15:59 -0500

    continued proj setup

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/main.handlebars')))
 
 app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+    res.send('GET request to the homepage')
 })
 
 app.listen(PORT, () => console.log(`app listening on PORT ${PORT}!`))
