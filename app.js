3bf2c06ee0a1a0c6982a340b70e8e92f61718385
Jesie Oldenburg <kitglo46@gmail.com>
2019-05-24 15:48:49 -0500

    continuing to add to API logic; Creating & testing connections

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
@@ -8,15 +9,17 @@ require('./controllers')(app);
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded())
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('/', (req, res) => {
-    console.log(`GET REQUEST`);
     res.render('landing');
 });
 
+app.post('/users', (req, res) => {
+    return res.send('POST Request to API homes');
+});
+
 app.listen(PORT, () => console.log(`app listening on PORT ${PORT}!`));
-module.exports = app;
--- controllers/sr_apiCalls.js
+++ controllers/sr_apiCalls.js
@@ -6,14 +6,16 @@ module.exports = function(req, res, app) {
     const URL = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:426678/summary.json?api_key=${F1_API_KEY}`;
     console.log('INSIDE POST FUNC!!')
 
+    app.get('/', (req, res) => {
+        res.send(axios.get(URL)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error(err);
+            }));
+    });
 
-    axios.get(URL)
-        .then(res => {
-            console.log(res);
-        })
-        .catch(err => {
-            console.error(err);
-        })
 
 
 };
