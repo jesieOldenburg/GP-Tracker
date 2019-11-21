549814fe11f437a801b98998d4c78b5cf0f052c0
Jesie Oldenburg <oldesdev@outlook.com>
2019-11-20 23:41:25 -0600

    continuing to refactor logic for external data population into card elements

--- controllers/formatExtData.js
+++ controllers/formatExtData.js
@@ -3,22 +3,32 @@ let Competitor = require('../models/f1Data'); // This module holds all of the Cl
 var COMP_ARRAY = [];
 var RACE_ROUNDS = []; // This is an array with every stage. Use the stage's id key , then the competitor object to access the stage results object... 
 var RESULTS_ARRAY = [];
+var axios = require('axios');
+let F1_API_KEY = process.env.SPT_RADAR_API_KEY;
 
-function formatCompetitors(data) {
+function formatCompetitors(data, URL) {
+    var STAGES_IDs = data.stage.stages;
+    // console.log("Called Competitor", STAGES_IDs)
+    
     var competitors = data.stage.competitors; // Returns a JSON obj with each competitors profile...
+    // console.log('competitors', competitors)
     for (const driver in competitors) {
         if (competitors.hasOwnProperty(driver)) {
             // Drivers Data : 
+            // console.log(driver)
             const driver_id = competitors[driver].id;
             const driver_name = competitors[driver].name;
             const driver_cnt = competitors[driver].country_code;
             const driver_team = competitors[driver].team.name;
-
-            var Driver = new Competitor(driver_id, driver_name, driver_cnt, driver_team);
+            const driver_pnts = competitors[driver].result.points;
+            const driver_pos = competitors[driver].result.position;
+            // console.log('driver_pnts', driver_pnts)
+            
+            var Driver = new Competitor(driver_id, driver_name, driver_cnt, driver_team, driver_pnts);
             COMP_ARRAY.push(Driver);
         }
     }
-    // console.log("TCL: formatCompetitors -> COMP_ARRAY", COMP_ARRAY)
+    console.table(COMP_ARRAY)
     return COMP_ARRAY;
 }
 
@@ -42,8 +52,8 @@ function formatTeams(data) { // Data is passed from the axios call within viewRo
     console.log('Here are the teams ==>', teams);
 }
 
-function formatData(data) {
-    formatCompetitors(data);
+function formatData(data, URL) {
+    formatCompetitors(data, URL);
 }
 
 module.exports = formatCompetitors, formatData;
--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -13,21 +13,19 @@ module.exports = function(app) {
     });
 
     app.get('/landing', (req, res) => { // Once authenticated, get the 'landing' HBS template.
+        axios.get(URL)
+            .then(res => {
+                console.log('response =====>>', res.data)
+            })
+            .catch(err => {
+                console.error(err);
+            })
         res.render('landing');
-        // axios.get(URL) // Call the API...
-        //     .then(res => {
-        //         var masterData = res.data;
-        //         return formatData(masterData);
-        //     })
-        //     .then((dataArray) => {
-        //         res.render('landing', { dataArray });
-        //     })
-        //     .catch(err => { console.error("FAILED GET REQ", err) });
-        // getData();
     });
 
     app.get('/drivers', (req, res) => {
-        URL = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:426678/summary.json?api_key=${F1_API_KEY}`;
+        URL = `https://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:426678/summary.json?api_key=${F1_API_KEY}`; //this endpoint retrieves the whole season...
+        console.log('got drivers \n')
         axios.get(URL) // Call the API...
             .then(res => {
                 var masterData = res.data;
@@ -43,5 +41,19 @@ module.exports = function(app) {
     app.get('/teams', (req, res) => {
 
 
+    });
+
+    app.get('/standings', (req, res) => {
+        // for (let i = 0, len = Stages.length; i < len; i++) {
+
+        // }
+        axios.get(URL)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.error(err);
+            })
+        res.render('standings')
     });
 }
